5cca34efc4738eaba2ccb82421e10be4815b54a9
Eduardo Viudes <duduzinhochorro@gmail.com>
2024-06-17 16:34:19 -0300

    links de instagram e linkedin

--- site/src/pages/Cards.jsx
+++ site/src/pages/Cards.jsx
@@ -5,8 +5,8 @@ export default function Cards(){
                 <img src="../../public/ana.png" alt="" className="w-40 h-40 rounded-full mb-3"/>
                 <h1 className="mb-3 text-2xl text-spectrum">Ana Oliveira</h1>
                 <div className="flex flex-row space-x-3">
-                    <a href="https://www.instagram.com/foxpectrum/?utm_source=ig_web_button_share_sheet"><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
-                    <img src="../../public/Group 2 1.png" alt="" className="w-12"/>
+                    <a href="https://www.instagram.com/nanagoround_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
+                    <a href=""><img src="../../public/Group 2 1.png" alt="" className="w-12"/></a>
                 </div>
             </div>
 
@@ -14,8 +14,8 @@ export default function Cards(){
                 <img src="../../public/viudes.png" alt="" className="w-40 h-40 rounded-full mb-3"/>
                 <h1 className="mb-3 text-2xl text-spectrum">Eduardo Viudes</h1>
                 <div className="flex flex-row space-x-3">
-                    <a href="https://www.instagram.com/foxpectrum/?utm_source=ig_web_button_share_sheet"><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
-                    <img src="../../public/Group 2 1.png" alt="" className="w-12"/>
+                    <a href="https://www.instagram.com/viudes_ch/?utm_source=ig_web_button_share_sheet"><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
+                    <a href="https://www.linkedin.com/in/eduardo-chorro-0978ba28b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img src="../../public/Group 2 1.png" alt="" className="w-12"/></a>
                 </div>
             </div>
 
@@ -23,8 +23,8 @@ export default function Cards(){
                 <img src="../../public/julia.png" alt="" className="w-40 h-40 rounded-full mb-3"/>
                 <h1 className="mb-3 text-2xl text-spectrum">Julia Leal</h1>
                 <div className="flex flex-row space-x-3">
-                    <a href="https://www.instagram.com/foxpectrum/?utm_source=ig_web_button_share_sheet"><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
-                    <img src="../../public/Group 2 1.png" alt="" className="w-12"/>
+                    <a href="https://www.instagram.com/lju.gomes?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
+                    <a href="https://www.linkedin.com/in/j%C3%BAlia-leal-benevides-gomes-4a128224a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img src="../../public/Group 2 1.png" alt="" className="w-12"/></a>
                 </div>
             </div>
 
@@ -32,8 +32,8 @@ export default function Cards(){
                 <img src="../../public/laura.png" alt="" className="w-40 h-40 rounded-full mb-3"/>
                 <h1 className="mb-3 text-2xl text-spectrum">Laura Kalybatas</h1>
                 <div className="flex flex-row space-x-3">
-                    <a href="https://www.instagram.com/foxpectrum/?utm_source=ig_web_button_share_sheet"><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
-                    <img src="../../public/Group 2 1.png" alt="" className="w-12"/>
+                    <a href="https://www.instagram.com/laurakalybatas/?utm_source=ig_web_button_share_sheet"><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
+                    <a href=""><img src="../../public/Group 2 1.png" alt="" className="w-12"/></a>
                 </div>
             </div>
 
@@ -41,8 +41,8 @@ export default function Cards(){
                 <img src="../../public/pedro.png" alt="" className="w-40 h-40 rounded-full mb-3"/>
                 <h1 className="mb-3 text-2xl text-spectrum">Pedro Araújo</h1>
                 <div className="flex flex-row space-x-3">
-                    <a href="https://www.instagram.com/foxpectrum/?utm_source=ig_web_button_share_sheet"><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
-                    <img src="../../public/Group 2 1.png" alt="" className="w-12"/>
+                    <a href="https://www.instagram.com/p.araujozr?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
+                    <a href="https://www.linkedin.com/in/pedro-ara%C3%BAjo-8a054b314"><img src="../../public/Group 2 1.png" alt="" className="w-12"/></a>
                 </div>
             </div>
 
@@ -51,7 +51,7 @@ export default function Cards(){
                 <h1 className="mb-3 text-2xl text-spectrum">Yannie Yshin</h1>
                 <div className="flex flex-row space-x-3">
                     <a href="https://www.instagram.com/foxpectrum/?utm_source=ig_web_button_share_sheet"><img src="../../public/icon insta.png" alt="" className="w-12"/></a>
-                    <img src="../../public/Group 2 1.png" alt="" className="w-12"/>
+                    <a href=""><img src="../../public/Group 2 1.png" alt="" className="w-12"/></a>
                 </div>
             </div>
 
